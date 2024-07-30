1d272cbea3425285d28d2816aca36ba12ecc0c3a
Pedro Alquimim <pedrohalquimim@gmail.com>
2024-07-30 00:27:03 -0300

    :whale: chore(docker): setup docker to the app and db

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { env } from './@env'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const PORT = env.PORT
-  await app.listen(PORT)
+  await app.listen(PORT, () =>
+    console.log(`Running API in MODE: ${env.NODE_ENV} on POST: ${env.PORT}`),
+  )
 }
 bootstrap()
