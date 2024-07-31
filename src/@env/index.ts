8690ca86e668ff5b5249b8b2f77c694f2ab7aa67
Pedro Alquimim <pedrohalquimim@gmail.com>
2024-07-30 23:39:13 -0300

    :wrench: feat(database): add typeORM with setup to postgres

--- src/@env/index.ts
+++ src/@env/index.ts
@@ -7,6 +7,10 @@ const envSchema = z.object({
     .default('development'),
   PORT: z.coerce.number().default(3000),
   JWT_SECRET_PASSWORD: z.string(),
+  DB_PORT: z.coerce.number().default(5432),
+  DB_USERNAME: z.string(),
+  DB_PASSWORD: z.string(),
+  DB_DATABASE: z.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { env } from './@env'
+import ormConfig from './infrastructure/config/orm.config'
+import ormConfigProd from './infrastructure/config/orm.config.prod'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [ormConfig],
+      expandVariables: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: env.NODE_ENV !== 'production' ? ormConfig : ormConfigProd,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
