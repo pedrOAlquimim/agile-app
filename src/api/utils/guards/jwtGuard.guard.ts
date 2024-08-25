dbfb536bd2171d7ef2140f71cd9752dfd1cdb91e
Pedro Alquimim <pedrohalquimim@gmail.com>
2024-08-24 21:12:09 -0300

    :sparkles: feat: create jwt guard

---
+++ src/api/utils/guards/jwtGuard.guard.ts
@@ -0,0 +1,39 @@
+import { Request } from 'express'
+import { JwtService } from '@nestjs/jwt'
+import { env } from 'src/@env'
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common'
+
+@Injectable()
+export class JwtGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest()
+    const token = this.extractTokenFromRequest(request)
+
+    if (!token) throw new UnauthorizedException()
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: env.JWT_SECRET_KEY,
+      })
+
+      request['user'] = payload
+    } catch (error) {
+      throw new UnauthorizedException()
+    }
+
+    return true
+  }
+
+  private extractTokenFromRequest(request: Request) {
+    const [type, token] = request.headers.authorization.split(' ') ?? []
+
+    return type === 'Bearer' ? token : undefined
+  }
+}
